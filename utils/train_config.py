a252b0eba80b9f58ca8b3885ca5160174b1443e1
6uvsoom <102727688+jwoosang1@users.noreply.github.com>
2025-09-30 19:09:42 +0900

    Update train_config.py

--- utils/train_config.py
+++ utils/train_config.py
@@ -1,62 +1,54 @@
+# utils/train_config.py
 """
-Set training options.
+Set training options for paper-release.
 """
 import copy
-import os
-import pickle
-
-import torch
 from utils.transforms_utils import transform, augmentation
 
 args = dict(
-    
     save=True,
     checkpoint='exp/checkpoint.pth',
-    save_name ='exp_test',
+    save_name='exp_paper',
     save_path='exp',
 
-    crop = 'tomato2',
-    
-    src_dataset = {
+    crop='apple',
+    src_dataset={
         'kwargs': {
-            'type': 'src_bg_augmented',
-            #'type': 'src_lab', 
+            'type': 'src_bg_augmented',  # or 'src_lab'
             'transform': transform,
         },
-        'batch_size' : 64,
-        'num_workers' : 4,
+        'batch_size': 64,
+        'num_workers': 4,
     },
-    
-    tgt_dataset = {
+
+    tgt_dataset={
         'kwargs': {
             'type': 'tgt',
             'transform': transform,
             'augmentation': augmentation,
         },
-        'batch_size' : 64,
-        'num_workers' : 4,
+        'batch_size': 64,
+        'num_workers': 4,
     },
 
-    test_dataset = {
+    test_dataset={
         'kwargs': {
             'type': 'tst',
             'transform': transform,
         },
-        'batch_size' : 64,
-        'num_workers' : 4,
+        'batch_size': 64,
+        'num_workers': 4,
     },
 
-    model = {
-        'name': 'dann',
-        'backbone' : 'resnet18',
-        'n_class' : 3,
+    model={
+        'name': 'dann',       # ddc | dcoral | dann | cdan | daln
+        'backbone': 'resnet18',
+        'n_class': 3,
     },
 
-    n_epochs = 300,
-    early_stop = 300
-    ,
+    n_epochs=300,
+    early_stop=25,
 )
 
 def get_args():
-  return copy.deepcopy(args)
-
+    return copy.deepcopy(args)
